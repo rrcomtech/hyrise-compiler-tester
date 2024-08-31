e12dfe9a55c2d5ef7ecba1fc8ecb9cd87281eefa
Robert Richter <robert.richter@rrcomtech.com>
2024-08-31 14:56:38 +0200

    Added missing --recursive call

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ release_compilation_directory="cmake-release-$nix_package"
 
 # Clone Hyrise
 mkdir $temp_dir && cd $temp_dir
-nix-shell -p git --run "git clone $hyrise_url --depth 1 --branch master $temp_dir"
+nix-shell -p git --run "git clone $hyrise_url --recursive --depth 1 --branch master $temp_dir"
 cd $temp_dir
 
 mkdir $debug_compilation_directory $release_compilation_directory
